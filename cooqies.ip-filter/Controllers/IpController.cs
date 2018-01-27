b6f3117e37c202b18edc4a65f2e2f641af103bcd
lizasixsixsix <lizasixsixsix@gmail.com>
2018-01-27 15:26:10 +0300

    IP filter implemented

---
+++ cooqies.ip-filter/Controllers/IpController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Http;
+using cooqies.ip_filter.Filters;
+
+namespace cooqies.ip_filter.Controllers
+{
+    public class IpController : ApiController
+    {
+        // GET: Ip
+        [IpFilter]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult Index()
+        {
+            string ip = String.Empty;
+            object _ip;
+
+            if (Request.Properties.TryGetValue("Ip", out _ip))
+            {
+                ip = (string)_ip;
+            }
+
+            return Ok(ip);
+        }
+    }
+}
---
+++ cooqies.ip-filter/Filters/IpFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace cooqies.ip_filter.Filters
+{
+    public class IpFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var ip = GetClientIpAddress(actionContext.Request);
+
+            actionContext.Request.Properties.Add(new KeyValuePair<string, object>("Ip", ip));
+
+            base.OnActionExecuting(actionContext);
+        }
+
+        private string GetClientIpAddress(HttpRequestMessage request)
+        {
+            if (request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                return IPAddress.Parse(((HttpContextBase)request.Properties["MS_HttpContext"]).Request.UserHostAddress).ToString();
+            }
+
+            return String.Empty;
+        }
+    }
+}
